274694451941f458da65d3e314126e26a371e02a
Joel Lopez <jolopez.satx@gmail.com>
2023-10-04 08:54:59 -0500

    added reset-button and added our github profiles

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-// Hidden Gems
-var gemsButton = document.getElementById("hiddenGems");
-var subtitle = document.querySelector(".subtitle")
-var main = document.getElementById("mainContainer")
-var gems = document.getElementById("gems")
-var food = document.getElementById("food")
-var local = document.getElementById("local")
+var subtitle = document.querySelector(".subtitle");
+var main = document.getElementById("mainContainer");
+var gems = document.getElementById("gems");
+var food = document.getElementById("food");
+var local = document.getElementById("local");
+var resetButton = document.getElementById("resetButton");
+//created variables to use in resetbutton function
+var initialGemsHTML = gems.innerHTML;
+var initialFoodHTML = food.innerHTML;
+var initialLocalHTML = local.innerHTML;
+//grabbed the button variables and put them in a function
+//grabbed eventlisteners and put them in a function
+//therefor, when we click on the resetbutton, "setupeventlisteners" is called and helps perform the task.
+function setUpEventListeners() {
+  var gemsButton = document.getElementById("hiddenGems");
+  var foodButton = document.getElementById("localRestaurants");
+  var trucksButton = document.getElementById("foodTrucks");
 
-gemsButton.addEventListener("click", hiddenGems)
-    function hiddenGems(){
+  gemsButton.addEventListener("click", hiddenGems);
+  trucksButton.addEventListener("click", trucks);
+  foodButton.addEventListener("click", localRest);
+}
+//call function
+setUpEventListeners();
 
-        food.innerHTML = ""
-        local.innerHTML = ""
-    }
+resetButton.addEventListener("click", function () {
+  gems.innerHTML = initialGemsHTML;
+  food.innerHTML = initialFoodHTML;
+  local.innerHTML = initialLocalHTML;
 
+  setUpEventListeners();
+});
 
-
-
-
-
+//hidden Gems
+function hiddenGems() {
+  food.innerHTML = "";
+  local.innerHTML = "";
+}
 // Food Trucks
-var trucksButton = document.getElementById("foodTrucks");
 
-trucksButton.addEventListener("click", trucks)
-    function trucks(){
-
-        gems.innerHTML = ""
-        local.innerHTML = ""
-    }
-
-
-
-
-
-
-
-
+function trucks() {
+  gems.innerHTML = "";
+  local.innerHTML = "";
+}
 
 //Local Restaurants
-var foodButton = document.getElementById("localRestaurants");
 
-foodButton.addEventListener("click", localRest)
-    function localRest(){
-
-        gems.innerHTML = ""
-        food.innerHTML = ""
-    }
+function localRest() {
+  gems.innerHTML = "";
+  food.innerHTML = "";
+}
