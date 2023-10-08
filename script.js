338f03ac43d77e461ab6d1cbe9a6c4121f9847ef
Joel Lopez <jolopez.satx@gmail.com>
2023-10-07 22:54:56 -0500

    added new card with search tab and API for nutrition

--- script.js
+++ script.js
@@ -1,126 +1,36 @@
-// var resetButton = document.getElementById("resetButton");
+var myButton = document.getElementById("searchBtn");
+var queryInput = document.getElementById("foodSearch");
+var nutrition = document.getElementById("nutrition");
 
-// resetButton.addEventListener("click", function () {
-//   gems.innerHTML = initialGemsHTML;
-//   food.innerHTML = initialFoodHTML;
-//   local.innerHTML = initialLocalHTML;
+myButton.addEventListener("click", function () {
+  search();
+});
 
-//   setUpEventListeners();
-// });
-
-// // Hidden Gems
-// var gemsButton = document.getElementById("hiddenGems");
-// var subtitle = document.querySelector(".subtitle")
-// var main = document.getElementById("mainContainer")
-// var gems = document.getElementById("gems")
-// var food = document.getElementById("food")
-// var local = document.getElementById("local")
-
-// gemsButton.addEventListener("click", hiddenGems)
-//     function hiddenGems(){
-
-//         food.innerHTML = ""
-//         local.innerHTML = ""
-//     }
-
-
-
-
-
-
-// // Food Trucks
-// var trucksButton = document.getElementById("foodTrucks");
-
-// trucksButton.addEventListener("click", trucks)
-//     function trucks(){
-
-//         gems.innerHTML = ""
-//         local.innerHTML = ""
-//     }
-
-
-
-
-
-
-
-
-
-// //Local Restaurants
-// var foodButton = document.getElementById("localRestaurants");
-
-// foodButton.addEventListener("click", localRest)
-//     function localRest(){
-
-//         gems.innerHTML = ""
-//         food.innerHTML = ""
-  
-//         // var option1 = document.getElementById("option1")
-// // var option2 = document.getElementById("option2")
-// // var option3 = document.getElementById("option3")
-// // var hiddenGems = [{
-// //          option1.innerHTML = "Family Friendly"
-// // }
-//     }
-
-//server side API
-//tried google maps, blocked by CORS
-/*var apiKey = 'AIzaSyDUJ6rFl2TKfn2JOTYQFnTv7uj1XUF3k9Q';
-var query = 'restaurants in San Antonio'; 
-
-var apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${apiKey}`;
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Process the data here
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });*/
-
-//nutrition API Key
-  var query = '1lb of lean beef'
-$.ajax({
+function search() {
+  var query = queryInput.value;
+  $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/nutrition?query=' + query,
-    headers: { 'X-Api-Key': 'hIMS2Z0KH5Jqeiuugextrw==x2YQ5ZZOi7KaZqO1'},
+    headers: { 'X-Api-Key': 'u+4h51GAe7l7Bv3AmACSaQ==ERvGRsarbvzIFnGJ' },
     contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
+    success: function (result) {
+      try {
+        console.log(result); 
+          var foodData = result[0];
+          if (foodData.name) {
+            var foodName = foodData.name;
+            var calories = foodData.calories;
+            var protein = foodData.protein_g;
+            var sugar = foodData.sugar_g;
+            nutrition.innerHTML = `Name: ${foodName}<br> Calories: ${calories}<br>Protein: ${protein}g<br>Sugar: ${sugar}g`;
+          } 
+      } catch (error) {
+        nutrition.innerHTML = "Error";
+        console.error('Error parsing JSON: ', error);
+      }
     },
     error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
+      console.error('Error: ', jqXHR.responseText);
     }
-});
-
-//Historical API Key
-var text = 'the alamo'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/historicalevents?text=' + text,
-    headers: { 'X-Api-Key': 'hIMS2Z0KH5Jqeiuugextrw==x2YQ5ZZOi7KaZqO1'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
-
-//Coctails
-var name = 'bloody mary'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/cocktail?name=' + name,
-    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+  });
+}
